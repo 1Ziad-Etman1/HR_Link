feafb06e4eefd485a9a570b6a7197def695e4c48
EslamAhmed171 <102628124+EslamAhmed171@users.noreply.github.com>
2023-04-17 23:00:28 +0200

    new js

--- website Pages/secTable.js
+++ website Pages/secTable.js
@@ -1,12 +1,13 @@
 var empolyees = JSON.parse(localStorage.getItem('empolyees'));
+var secEmpolyees = JSON.parse(localStorage.getItem('secEmpolyees'));
 // if empty
-if (empolyees == null){
-    empolyees = [];
+if (secEmpolyees == null){
+    secEmpolyees = [];
 }
 var table = document.getElementById('table');
 var tableData = document.getElementsByTagName('td');
 var tableRows = document.getElementsByTagName('tr');
-for (empolyee of empolyees) {
+for (empolyee of secEmpolyees) {
     let template = 
                 `<tr>
                     <td>${empolyee.id}</td>
@@ -23,30 +24,30 @@ for (empolyee of empolyees) {
                     <td><button id="delete${empolyees.indexOf(empolyee)}">delete</button></td>
                 </tr>`;
     
-    table.innerHTML += template;
+    table.innerHTML += template
 }
+
 // update action
-for (let i = 0; i < empolyees.length; i++) {
+for (let i = 0; i < secEmpolyees.length; i++) {
     var btn = document.getElementById(`update${i}`);
     btn.addEventListener('click', function(e) {
         e.preventDefault();
-        localStorage.setItem('autoFill',JSON.stringify(empolyees[i]));
+        localStorage.setItem('autoFill',JSON.stringify(secEmpolyees[i]));
         window.open('update.html');
     })
 }
 // delete action
-for (let i = 0; i < empolyees.length; i++) {
+for (let i = 0; i < secEmpolyees.length; i++) {
     var btn = document.getElementById(`delete${i}`);
     btn.addEventListener('click', function(e) {
         e.preventDefault();
         empolyees.splice(i, 1);
         localStorage.setItem('empolyees',JSON.stringify(empolyees));
+        secEmpolyees.splice(i, 1);
+        localStorage.setItem('empolyees',JSON.stringify(secEmpolyees));
         location.reload();
     })
 }
-// function deleteEmpolyee(idx){
-//     console.log(idx);
-//     empolyees = empolyees.splice(idx, 1);
-//     localStorage.setItem('empolyees', JSON.stringify(empolyees));
-//     location.reload();
-// }
+
+
+localStorage.removeItem('secEmpolyees');
